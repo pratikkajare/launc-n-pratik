7c6463780bc6c665e7f96dc1eb8e3e0d5d163e97
pratikkajare <pratikkajare826@gmail.com>
2024-06-17 12:29:08 +0530

    feat: common folder live on npm 3

--- dist/auth/index.js
+++ dist/auth/index.js
@@ -14,7 +14,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./guards/access-token.guard"), exports);
-__exportStar(require("./guards/refresh-token.guard"), exports);
-__exportStar(require("./guards/roles.guard"), exports);
-__exportStar(require("./strategies/access-token.strategy"), exports);
+__exportStar(require("./guards"), exports);
+__exportStar(require("./strategies"), exports);
--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,4 +1,2 @@
-export * from "./guards/access-token.guard";
-export * from "./guards/refresh-token.guard";
-export * from "./guards/roles.guard";
-export * from "./strategies/access-token.strategy";
+export * from "./guards";
+export * from "./strategies";
